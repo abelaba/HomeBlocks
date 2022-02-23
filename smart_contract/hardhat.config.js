54a79bf449462a0d3431f0e925226b19c3fdd302
abelaba <se.abel.abate@gmail.com>
2022-02-24 00:55:41 +0300

    Configured hardhat network

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -2,10 +2,10 @@ require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: '0.8.0',
-  // networks: {
-  //   ropsten: {
-  //     url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
-  //     accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
-  //   },
-  // },
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      accounts: ['0x05c368d4bfd4821a770cca8c143cff2605cb8e13023b1dfc9c876341840ec344'],
+    }
+  },
 };
