f81052fcca09950e0dd353fac4548e767f80b0cd
abelaba <se.abel.abate@gmail.com>
2023-02-24 02:35:29 +0300

    Property card display layout fixed

--- client/src/components/AddPropertyCard.jsx
+++ client/src/components/AddPropertyCard.jsx
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { BsFillHouseAddFill } from "react-icons/bs";
 
 export default function AddPropertyCard() {
-    const address = "https://cdn4.iconfinder.com/data/icons/vectory-bonus-3/40/button_add-512.png"
-    return <Link to="/addProperty">
-        <div class="p-10">
-            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-indigo-500 hover:bg-violet-400">
-                <img class="w-full" src={address} alt="Mountain" />
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl text-white mb-2">Add Property</div>
-                    <p class="text-white text-base">
-                    </p>
-                </div>
-            </div>
+  
+  return (
+    <Link to="/addProperty">
+      <div className="p-4 rounded overflow-hidden shadow-lg bg-yellow-500 hover:bg-violet-400 grid grid-flow-cols mb-6">
+        <div className="grid items-center justify-center">
+          <div className="font-bold text-xl text-black">
+            <BsFillHouseAddFill size={20} className="inline" /> Add Property
+          </div>
         </div>
+      </div>
     </Link>
+  );
 }
--- client/src/components/PropertyCard.jsx
+++ client/src/components/PropertyCard.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { PORT } from '../utils/constants';
-import { shortenAddress } from '../utils/shortenAddress';
-export default function PropertyCard ({property}){
-    console.log(`https://localhost:${PORT}property.rentalImage`);
-    const address = "https://media.istockphoto.com/photos/beautiful-luxury-home-exterior-on-bright-sunny-day-with-green-grass-picture-id1223022367?s=2048x2048"
-    return (
-        <div className='max-w-md'>
-            <Link to={`/property/${property.propertyCount}`}>
-                <div class=" rounded overflow-hidden shadow-lg bg-indigo-500 hover:bg-violet-400">
-                <img class="w-full" src={`http://localhost:${PORT}/${property.rentalImage}`} alt="House"/>
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl text-white mb-2">{property.name}</div>
-                    <p class="text-white text-base">
-                        {property.description}
-                    </p>
-                </div>
-                <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{property.timestamp}</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{shortenAddress(property.owner)}</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{property.address}</span>
-                </div>
-                </div>
-            </Link>
+import React from "react";
+import { Link } from "react-router-dom";
+import { PORT } from "../utils/constants";
+import { shortenAddress } from "../utils/shortenAddress";
+import { MdLocationPin } from "react-icons/md";
+
+export default function PropertyCard({ property }) {
+
+  return (
+    <div className="w-96">
+      <Link to={`/property/${property.propertyCount}`}>
+        <div className="rounded overflow-hidden shadow-lg bg-white hover:bg-violet-400 text-black">
+          <img
+            className="w-full"
+            src={`http://localhost:${PORT}/${property.rentalImage}`}
+            alt="House"
+          />
+          <div className="px-6 pt-4 ">
+            <div className="font-bold text-xl mb-2">
+              <span className="text-black-400 mr-1">{property.price}</span>ETH /
+              month
+            </div>
+          </div>
+          <div className="px-6 py-2 border-b-2 mx-2">
+            <div className="font-bold text-xl mb-2">{property.name}</div>
+            <p className="text-base">
+              <MdLocationPin className="inline" /> {property.address}
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              {property.timestamp}
+            </span>
+            <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              {shortenAddress(property.owner)}
+            </span>
+
+            <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              Bedrooms: {property.numBedrooms}
+            </span>
+            <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              Bathrooms: {property.numBathrooms}
+            </span>
+            <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              Area: {property.totalArea} sqft
+            </span>
+          </div>
         </div>
-    )
+      </Link>
+    </div>
+  );
 }
