7b1b06f03a7025c737eb64efb0e00a159efb2631
abelaba <se.abel.abate@gmail.com>
2023-03-29 11:05:29 +0300

    Property Detail UI layout changed

--- client/src/components/PropertyDetail.jsx
+++ client/src/components/PropertyDetail.jsx
@@ -1,57 +1,132 @@
 import { useParams } from "react-router-dom";
-import {useContext,useEffect,useState} from "react";
+import { useContext, useEffect, useState } from "react";
 import { PropertyHandlingContext } from "../context/PropertyHandlingContext";
 import { AuthenticationContext } from "../context/AuthenticationContext";
-import { PORT,SECRET } from "../utils/constants";
-import {decodeToken } from "react-jwt";
+import { PORT } from "../utils/constants";
+import { decodeToken } from "react-jwt";
 import { ChatContext } from "../context/ChatContext";
 import { FaEthereum } from "react-icons/fa";
+import {
+  MdOutlineBathroom,
+  MdOutlineBedroomParent,
+} from "react-icons/md";
+import { BiHome, BiSolidArea } from "react-icons/bi";
 
+export default function PropertyDetail() {
+  const { id } = useParams();
 
-export default function PropertyDetail(){
-    const{id} = useParams();
+  const { viewProperty } = useContext(PropertyHandlingContext);
+  const { token } = useContext(AuthenticationContext);
+  const { createChat } = useContext(ChatContext);
 
-    const {viewProperty} = useContext(PropertyHandlingContext);
-    const {token} = useContext(AuthenticationContext);
-    const {createChat} = useContext(ChatContext);
+  const [property, setProperty] = useState({});
+  const [userId, setUserId] = useState("");
 
-    const[property,setProperty] = useState("");
-    const[userId,setUserId] = useState("");
+  useEffect(async () => {
+    setProperty(await viewProperty(id));
 
-    useEffect(async () => {
-      setProperty(await viewProperty(id));
-      if(token){
-        const userId = decodeToken(token);
-        setUserId(userId._id);
-      }
-      }, []);
-    console.log(property);
-    console.log(id);
-    return <>
-    {property && 
-        <div>
-              <div class="p-10">
-                  <div class=" w-full flex flex-row">
-                    <div className="max-w-2xl"> <img src={`http://localhost:${PORT}/${property.rentalImage}`} /> </div>
-                    <div class="flex-auto bg-white rounded-b p-4 flex flex-col justify-between leading-normal">
-                      <div class="mb-8">
-                        <div class="text-gray-900 font-normal text-4xl pb-3 border-b-2 mb-2">House Listing</div>
-                        <div class="text-gray-900 font-bold text-xl mb-2">{property.name}</div>
-                        <div class="text-gray-900 text-md mb-2">Address: {property.address}</div>
-                        <div class="text-gray-900 text-md mb-2 border-2 rounded p-10">{property.description}</div>
-                        <div class="text-gray-900 text-2xl flex flex-row gap-x-2 items-baseline"> <p>Price:</p> <p>{property.price} ETH</p> <FaEthereum className="p-0"/> </div>
-                        <div class="text-gray-900 text-xl flex flex-row gap-x-2 items-baseline"> <p>Owner:</p> <p>{property.owner}</p> </div>
-                        <div class="text-gray-900 text-xl flex flex-row gap-x-2 items-baseline"> <p>Date Listed:</p> <p>{property.timestamp}</p> </div>
-                      </div>
-                      <div class="flex items-center">
-                      {userId && userId!=property.userId && <button onClick={()=>createChat(property.userId, property.propertyCount)} class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 hover:border-blue-500 rounded-lg">
+    if (token) {
+      const userId = decodeToken(token);
+      setUserId(userId._id);
+    }
+  }, []);
+
+  return (
+    <div>
+      {property && (
+        <div className="p-10">
+          <div className="w-full flex flex-row bg-white p-5 rounded-t-md grid grid-cols-2 ">
+            <div className="flex-auto rounded-b p-4 flex flex-col justify-between leading-normal">
+              <div className="mb-8">
+                <div className="border-b-2 flex items-center justify-between">
+                  <div>
+                    <div className="text-gray-900 font-normal font-bold text-4xl">
+                      {property.name}
+                    </div>
+                    <div className="text-gray-900 font-normal text-xl pb-3 mb-2 ml-1">
+                      {property.address}
+                    </div>
+                  </div>
+                  <div>
+                    {userId && userId !== property.userId && (
+                      <button
+                        onClick={() =>
+                          createChat(property.userId, property.propertyCount)
+                        }
+                        className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 hover:border-blue-500 rounded-lg"
+                      >
                         Talk with owner
-                        </button>}
+                      </button>
+                    )}
+                  </div>
+                </div>
+
+                <div className="pl-3">
+                  <div className="text-gray-900 text-2xl flex flex-row gap-x-2 items-baseline">
+                    <p className="font-bold my-4">
+                      {property.price} ETH <FaEthereum className="p-0 inline" />{" "}
+                      /month
+                    </p>
+                  </div>
+                  <div className="text-gray-900 text-xl mb-3">
+                    <div className="flex items-center gap-x-2">
+                      <p className="font-semibold text-gray-600">Listed:</p>
+                      <p className="text-gray-800">
+                        {new Date(property.timestamp).toLocaleDateString()}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="text-gray-900 text-xl my-6">
+                    <div className="grid grid-cols-4 gap-6">
+                      <div className="flex items-center gap-x-2 mb-3">
+                        <p className="font-semibold text-gray-600">
+                          <BiHome />{" "}
+                        </p>
+                        <p className="text-gray-800">{property.propertyType}</p>
+                      </div>
+                      <div className="flex items-center gap-x-2">
+                        <MdOutlineBedroomParent
+                          size={30}
+                          className="text-gray-600"
+                        />
+                        <p className="text-gray-800">
+                          {property.numBedrooms} Bedrooms
+                        </p>
+                      </div>
+                      <div className="flex items-center gap-x-2">
+                        <MdOutlineBathroom
+                          size={30}
+                          className="text-gray-600"
+                        />
+                        <p className="text-gray-800">
+                          {property.numBathrooms} Bathrooms
+                        </p>
+                      </div>
+                      <div className="flex items-center gap-x-2">
+                        <BiSolidArea size={30} className="text-gray-600" />
+                        <p className="text-gray-800">
+                          {property.totalArea} sqft
+                        </p>
                       </div>
                     </div>
                   </div>
+
+                  <div className="text-gray-900 text-md mb-2 border-2 rounded p-2">
+                    {property.description}
+                  </div>
+                </div>
               </div>
-      </div>
-  }
-    </>
+            </div>
+            <div className="max-w-2xl ">
+              <img
+                className="rounded-lg"
+                src={`http://localhost:${PORT}/${property.rentalImage}`}
+                alt="House"
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
