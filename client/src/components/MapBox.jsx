efb3a167731351cc96320e1a45a94ef29610e3f8
abelaba <se.abel.abate@gmail.com>
2023-04-20 02:27:36 +0300

    Map display pop up added

--- client/src/components/MapBox.jsx
+++ client/src/components/MapBox.jsx
@@ -1,71 +1,71 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWJlbGFiYSIsImEiOiJja3locWRhejgxcjNtMm51cm9zNHZjMGZ4In0.YUW_CcndK5LwRgTECN6WRA';
+import React, { useRef, useEffect, useState, useContext } from "react";
+import mapboxgl from "mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import { PropertyHandlingContext } from "../context/PropertyHandlingContext";
+import { PORT } from "../utils/constants";
 
 export default function MapBox() {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(38.90);
-    const [lat, setLat] = useState(8.94);
-    const [zoom, setZoom] = useState(3);
-    const data = [
-        {
-            "location": "Manhattan Ave & Norman Ave at NE corner",
-            "city": "Brooklyn",
-            "state": "New York",
-            "coordinates": [-73.9516030004786, 40.72557300071668],
-        },
-        {
-            "location": "6th Ave & 42nd St at NW corner",
-            "city": "Manhattan",
-            "state": "New York",
-            "coordinates": [-73.98393399979334, 40.75533300052329],
-        },
-        {
-            "location": "Essex St & Delancey St at SE corner",
-            "city": "Manhattan",
-            "state": "New York",
-            "coordinates": [-73.9882730001973, 40.718207001246554],
-        }
-    ]
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(38.9);
+  const [lat, setLat] = useState(8.94);
+  const [zoom, setZoom] = useState(1);
 
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/light-v10',
-            center: [lng, lat],
-            zoom: zoom
-        });
-        data.forEach((location) => {
-            console.log(location);
-            var marker = new mapboxgl.Marker()
-                .setLngLat(location.coordinates)
-                .setPopup(new mapboxgl.Popup({ offset: 30 })
-                    .setHTML('<h4>' + location.city + '</h4>' + location.location))
-                .addTo(map.current);
+  const { viewAllProperties } = useContext(PropertyHandlingContext);
 
-        })
+  useEffect(async () => {
+    if (!map.current) {
+      // Initialize the map only once
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/light-v10",
+        center: [lng, lat],
+        zoom: zoom,
+      });
+    }
+
+    // Load properties and set the state
+    const loadedProperties = await viewAllProperties();
+
+    // Add markers for each property
+    loadedProperties.forEach((property) => {
+      const { coordinates } = property;
+      // Create a marker
+      const marker = new mapboxgl.Marker()
+        .setLngLat(coordinates)
+        .setPopup(new mapboxgl.Popup({ offset: 30 }).setHTML(popUp(property))) // Customize the popup content
+        .addTo(map.current);
     });
+  }, []);
 
-    useEffect(() => {
-        if (!map.current) return; // wait for map to initialize
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
-        });
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
     });
+  });
 
-
-
-    return (
-        <div className='px-10 '>
-            <div className="sidebar">
-                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+  const popUp = (property) => {
+    return `
+            <div className="max-w-xl">
+                <a href=/property/${property.propertyCount}> 
+                    <img src=http://localhost:${PORT}/${property.rentalImage} alt="House" />
+                </a>
+                <p class="font-bold text-base mt-2">Price: ${property.price} ETH</p>  
             </div>
-            <div ref={mapContainer} className="map-container rounded shadow-lg shadow-cyan-500/50 bg-cyan-500" />
-        </div>
-    );
+        `;
+  };
+
+  return (
+    <div className="px-10 ">
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div
+        ref={mapContainer}
+        className="map-container rounded shadow-lg shadow-cyan-500/50 bg-cyan-500"
+      />
+    </div>
+  );
 }
