f1e416c8f8fe3ba2062f5c4d983ace8a3875742b
abelaba <se.abel.abate@gmail.com>
2023-08-30 22:50:39 +0300

    Removed unnecessary tracked files on server

--- server/app.js
+++ server/app.js
@@ -1,40 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
 const cors = require("cors");
 
-
 // * IMPORT ROUTES
-const authRoute = require('./routes/auth');
-const rentingRoute = require('./routes/renting');
-const chatRoute = require('./routes/chat.js');
+const authRoute = require("./routes/auth");
+const rentingRoute = require("./routes/renting");
+const chatRoute = require("./routes/chat.js");
 
 dotenv.config();
 
-
 // * CONNECT TO DB
 mongoose.connect(
-    // process.env.DB_connect,
-    'mongodb://127.0.0.1:27017/HomeBlocks',
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
-        console.log("*** DATABASE HAS CONNECTED SUCCESSFULLY");
-    })
-
-
+  process.env.DB_connect,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  () => {
+    console.log("*** DATABASE HAS CONNECTED SUCCESSFULLY");
+  }
+);
 
 app.use(express.json());
 app.use(cors());
-app.use('/uploads/', express.static('uploads'));
+app.use("/uploads/", express.static("uploads"));
 
+app.use("/api/user", authRoute);
+app.use("/api/rental", rentingRoute);
+app.use("/api/chat", chatRoute);
 
+const PORT = process.env.PORT || 5000;
 
-app.use('/api/user', authRoute);
-app.use('/api/rental',rentingRoute);
-app.use("/api/chat",chatRoute);
-
-PORT = 5000
 // * SERVER START
 app.listen(PORT, () => {
-    console.log(`*** SERVER IS RUNNING ON PORT ${PORT}`);
-})
+  console.log(`*** SERVER IS RUNNING ON PORT ${PORT}`);
+});
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,9 +1,20 @@
+const fs = require("fs");
+
 const main = async () => {
-  
-  const propertyFactory = await hre.ethers.getContractFactory("Property");
+  const propertyFactory = await hre.ethers.getContractFactory(
+    "PropertyContract"
+  );
   const propertyContract = await propertyFactory.deploy();
 
   await propertyContract.deployed();
+  try {
+    await fs.promises.copyFile(
+      "artifacts/contracts/PropertyContract.sol/PropertyContract.json",
+      "../client/src/utils/abi/PropertyContract.json"
+    );
+  } catch (error) {
+    console.log(error);
+  }
 
   console.log("PropertyContract address: ", propertyContract.address);
 };
