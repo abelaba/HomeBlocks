b40e3071ad761dcdab99904e8e0567f546d3cd96
abelaba <se.abel.abate@gmail.com>
2024-05-29 15:34:16 +0300

    Update dotenv enviroment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, ".env") });
+require("dotenv").config();
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary");
 
